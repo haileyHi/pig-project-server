134e79121bd01d643d460f91058380ad49d23fdd
haileyHi <yuhiro@naver.com>
2022-02-15 19:36:59 +0900

    :art: [FIX] Object into Any

--- src/main/java/com/whattoeat/fos/Util/Response.kt
+++ src/main/java/com/whattoeat/fos/Util/Response.kt
@@ -3,9 +3,9 @@ package com.whattoeat.fos.Util
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
-data class Response(val status : Int, val message: String, val data: Object?){
+data class Response(val status : Int, val message: String, val data: Any?){
     companion object {
-        fun newResult(status: HttpStatus, message: String?, result: Object?): ResponseEntity<Response> {
+        fun newResult(status: HttpStatus, message: String?, result: Any?): ResponseEntity<Response> {
             return if (result != null)
                 ResponseEntity(Response(status.value(), message!!, result), status)
             else
