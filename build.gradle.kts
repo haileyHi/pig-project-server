ee3f16e2854ab6d82951eb6e8ddd996147fdb948
haileyHi <yuhiro@naver.com>
2022-03-17 23:53:37 +0900

    update project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 tasks.getByName<Jar>("jar") {
     enabled = false
+}
+
+tasks.jar {
+    enabled = true
+    // Remove `plain` postfix from jar file name
+    archiveClassifier.set("")
 }
